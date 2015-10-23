28f8fd3ff14d069b7c2ff36f474a8564bda54018
Liu Chang <lllccc2009@gmail.com>
2015-10-23 17:20:39 -0400

    add baseline compile makefile and test file

---
+++ jni/Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+#opencv
+OPENCVROOT:= /home/changliu/OpenCV-2.4.9-android-sdk
+OPENCV_CAMERA_MODULES:=on
+OPENCV_INSTALL_MODULES:=on
+OPENCV_LIB_TYPE:=SHARED
+include ${OPENCVROOT}/sdk/native/jni/OpenCV.mk
+
+#add by chang
+LOCAL_CFLAGS := -DVL_DISABLE_SSE2 -DVL_DISABLE_AVX
+#add ends
+
+FILE_LIST := $(wildcard $(LOCAL_PATH)/vlfeat-0.9.19/src/*.c)
+FILE_LIST := $(wildcard $(LOCAL_PATH)/vlfeat-0.9.19/vl/*.c)
+FILE_LIST := $(FILE_LIST:$(LOCAL_PATH)/%=%)
+LOCAL_SRC_FILES := com_example_ndk_opencv_androidstudio_NativeClass.cpp
+LOCAL_SRC_FILES += $(FILE_LIST)
+LOCAL_LDLIBS += -llog -lm
+LOCAL_MODULE := MyLib
+
+include $(BUILD_SHARED_LIBRARY)
